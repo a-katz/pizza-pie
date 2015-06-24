a186e6d7420fa6ac6855be0aa4263f33d62a2d0e
Peter Reintjes <peter.reintjes@ncmls.org>
2015-06-23 21:17:11 -0400

    peter adds trackbar example

---
+++ traks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import numpy as np
+import cv2
+
+def nothing(x) :
+    pass
+
+cv2.namedWindow('foo')
+cv2.createTrackbar('thresh','foo',10,240,nothing)
+
+while(True) :
+    ch = cv2.waitKey(100)
+    if (ch == 27) :
+        exit()
+
